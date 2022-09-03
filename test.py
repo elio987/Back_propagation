0bb2130ae78c33095062e4dc0cce81029e1ec0e0
elio987 <leo_gracidam@hotmail.com>
2022-09-03 12:11:10 -0500

    second

--- test.py
+++ test.py
@@ -1,18 +1,23 @@
+#Leonardo Gracida Munoz A01379812
 import pickle
 import matplotlib.pyplot as plt
 import numpy as np
 
+#Abrimos la dataset de test
 with open('test.pkl', 'rb') as file:
     myvar = pickle.load(file)
+#Abrimos los pesos geenrados
 with open('model.pkl', 'rb') as file:
     myvar2 = pickle.load(file)
+
 X_test,y_test_cod = myvar
-print(X_test.shape)
 w0,w1,w2 = myvar2
 
+#Funcion de activacion de la neuronas
 def sigmoid(x):
     return(1/(1 + np.exp(-x)))
 
+#Funcion de perdida
 def loss(out, Y):
     s =(np.square(out-Y))
     #print(s)
@@ -20,6 +25,7 @@ def loss(out, Y):
     #print(np.sum(s),len(Y))
     return s
 
+#Funcion para predecir con la red entranada
 def feed_fwd(x,w0,w1,w2):
     h1 = np.dot(w0,x)
     a1 = sigmoid(h1)
@@ -29,6 +35,7 @@ def feed_fwd(x,w0,w1,w2):
     a3 = sigmoid(h3)
     return a3
 
+#Funcion que traduce la salida de la red a una label numerico
 def y_decof(y):
     if (y == np.array([[0,0,0,0]]).T).all():
         return 0.0
@@ -51,24 +58,33 @@ def y_decof(y):
     elif (y == np.array([[1,0,0,1]]).T).all():
         return 9.0
 
+#Obtenemos la accuray del modelo con el dataset de test
 l =[]
 for i in range(len(X_test)):
   out = feed_fwd(X_test[i], w0, w1, w2)
   l.append(loss(out, y_test_cod[i]))
 print("acc:", (1-(sum(l)/len(X_test)))*100," ======== loss:",sum(l)/len(X_test)) 
 
-fig = plt.figure(figsize=(25,25))
+#Ploteamos la unas cuantas imagenes y mostramos la predccion junto al label real
+fig = plt.figure()
 lugar = 0
-#index = [5, 100,250,300]
 for i in range(1, 7):
     plt.subplot(2, 3, i)
-    out = np.round(feed_fwd(X_test[lugar],w0,w1,w2))
+    #Predecimos
+    out = feed_fwd(X_test[lugar],w0,w1,w2)
+    for i in range(out.shape[0]):
+        if out[i] <= 0.4:
+            out[i] = 0
+        else:
+            out[i] = 1
+    #Lo pasamos a un label numerico
     out = y_decof(out)
+    #pasamos el label real a un label numerico
     y = y_decof(y_test_cod[lugar])
-    print(out,y)
+    print("Prediccion: ",out,", Real: ",y)
     plt.imshow(np.reshape(X_test[lugar],(8,8)))
-    #plt.plot(np.reshape(X_test,(8,8))*16)
+    plt.title("real: "+str(y)+", pred: "+str(out))
     lugar += 1
-    plt.text(-0.5, 0.5, "real: "+str(y)+", pred: "+str(out),
-             fontsize=10, ha='right')
+#Guardamos el modelo
+plt.savefig('predicciones.png')
 plt.show()
